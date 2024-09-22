86a6db0c0beb1f1e9b70af9155828a6d9ab19581
Aniket Singh <aniket.singh07vs@gmail.com>
2024-09-22 20:14:27 +0530

    Refactor studentLogin controller to handle login functionality and set JWT token in cookie

--- controllers/studentRegister.controller.js
+++ controllers/studentRegister.controller.js
@@ -123,27 +123,27 @@ const registerStudent = async (req, res) => {
 // };
 const studentLogin = async (req, res) => {
   try {
-    console.log(req.body);
     const { email, password } = req.body;
-
+    
     let user = await LoginStudent.findOne({ email });
-
+    
     if (!user) {
       return res.status(400).json({ message: "User does not exists" });
     }
     const passwordMatch = await bcrypt.compare(password, user.password);
-
+    
     if (!passwordMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
-
+    
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
     });
+    return res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: error.message });
